cd8fb78af07864b7bf2518779b9defcfe573e0bf
MRootxx <dr.rootvb@gmail.com>
2026-06-22 02:16:41 +0300

    fix: add email column migration to users table and add to fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'phone',
+        'email',
         'password',
         'profile_photo',
         'governorate_id',
