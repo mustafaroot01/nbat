5b448125a84b866f4fbfd32a9b3eecb3f234f6f9
MRootxx <dr.rootvb@gmail.com>
2026-06-22 00:59:27 +0300

    Run RolesAndPermissionsSeeder before AdminSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             GovernorateSeeder::class,
+            RolesAndPermissionsSeeder::class,
             AdminSeeder::class,
             SettingSeeder::class,
             UserSeeder::class,
